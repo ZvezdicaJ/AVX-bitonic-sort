263b281d12688897089354586ef67a8703edd435
Jure Lapajne <lapajne.jure@gmail.com>
2023-01-24 21:57:56 +0100

    More debug output for ci.

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -4,5 +4,5 @@ filename="benchmark_results.json"
 directory="results" 
 
 echo "writing to file: ${directory}/${filename}" 
-
+mkdir ${directory}
 ../install/Release/bin/benchmarks --benchmark_out="${directory}/${filename}" --benchmark_counters_tabular=true --benchmark_out_format=json 
