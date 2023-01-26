52a814bdeaeb6b0e0e02f0b0fcb0414ae4039437
Jure Lapajne <lapajne.jure@gmail.com>
2023-01-26 19:11:56 +0100

    Minor change in plot_results.py script.

--- benchmarks/plot_results.py
+++ benchmarks/plot_results.py
@@ -132,7 +132,7 @@ def plot_benchmarks(classification: BenchmarkClassification):
             ax.legend()
     fig.set_tight_layout(True)
     fig.show()
-    fig.savefig(f"./{benchmarks[0].element_type.name}_benchmarks.jpg")
+    fig.savefig(f"./results/{benchmarks[0].element_type.name}_benchmarks.jpg")
 
 
 def plot_speedup(classification: BenchmarkClassification):
